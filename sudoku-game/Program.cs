75167828c82b1b5fcf1e9291355d4d0017563480
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-03-27 19:00:00 -0700

    got rid of grid testing

--- sudoku-game/Program.cs
+++ sudoku-game/Program.cs
@@ -9,20 +9,6 @@ namespace sudoku_game
         static void Main(string[] args)
         {
             Grid testGrid = new Grid();
-
-            int count = 1;
-            for (int i = 0; i < testGrid.Tiles.Length; i++)
-            {
-                if (count++ % 3 == 0)
-                {
-                    Console.WriteLine(testGrid.Tiles[i]);
-                } else
-                {
-                    Console.Write(testGrid.Tiles[i]);
-                }
-                
-            }
-            Console.WriteLine($"The length is {testGrid.Tiles.Length}");
         }
     }
 }
