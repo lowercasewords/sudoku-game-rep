803cdb9d087edd9484936033757c5b7051e2c442
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-03-27 18:47:11 -0700

    * sudoku-game.sln: * Map.cs: * Grid.cs: * Program.cs: * sudoku-game.csproj:
    * .gitignore: Commiting from Visual Studio

---
+++ sudoku-game/Grid.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+class Grid
+{
+    /// <summary>
+    /// first element in the 'tiles' array represented in the game in
+    /// upper-left corner, the last element in bottom-right
+    /// </summary>
+    int?[] _tiles;
+    public int?[] Tiles
+    {
+        get
+        {
+            return _tiles;
+        }
+    }
+
+    public Grid() : this(9) { }
+    public Grid(int tileAmount) 
+    {
+        do
+        {
+            _tiles = new int?[tileAmount];
+            Regex excludePos = new Regex("[.]");
+            int randomPos;
+            for (int i = 0; i < _tiles.Length; i++)
+            {
+                randomPos = new Random().Next(1, 10);
+                /// <summary>
+                /// if the position was already taken,
+                /// repeat for-loop and exclude the position 
+                /// </summary>
+                if (excludePos.IsMatch(randomPos.ToString()))
+                {
+                    i--;
+                    Console.WriteLine("-");
+                    continue;
+                }
+                else
+                { AddToEndRegex(ref excludePos, randomPos); }
+
+                Console.WriteLine($"for-loop was completed {i} times");
+                Console.WriteLine($"random position: {randomPos}");
+                Console.WriteLine($"exclude position: {excludePos}");
+
+                _tiles[i] = randomPos;
+            } 
+        } while (
+ _tiles.Where(x => x != null).GroupBy(x => x).Any(x => x.Count() > 1)
+            );
+    }
+    private void AddToEndRegex<T>(ref Regex regex, T adding)
+    {
+        Console.WriteLine("Match was found");
+        regex = new Regex(
+                        regex.ToString().Substring(0, regex.ToString().Length - 1)
+                        + adding + ']');
+    }
+    public void Ensure()
+    {
+        
+    }
+}
---
+++ sudoku-game/Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace sudoku_game
+{
+    public class Map
+    {
+        Grid[] grids;
+
+        /// <summary>
+        ///  Creates a Map, note to me at start of project: be careful with Grid constructors,
+        ///  you might have changed the required paramaters
+        /// </summary>
+        public Map() : this(9) { }
+        public Map(int gridAmount)
+        {
+            if ((int)Math.Sqrt(gridAmount) != Math.Sqrt(gridAmount))
+                throw new Exception("A map cannot contain");
+            grids = new Grid[gridAmount];
+            for (int i = 0; i < gridAmount; i++)
+            {
+                grids[i] = new Grid();
+            }
+        }
+    }
+}
