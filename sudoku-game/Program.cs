dd7cbcba32e4169825eb074ce4ba7f0287ad4034
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-03-31 14:12:44 -0700

    * Program.cs:
    * Map.cs: Creating an algorigthms for filling Grids

--- sudoku-game/Program.cs
+++ sudoku-game/Program.cs
@@ -7,10 +7,32 @@ namespace sudoku_game
 {
     class Program
     {
+        private string a = "string a";
+        private string b = "string b";
+        private string c = "string c";
         static void Main(string[] args)
         {
-            Map map = new Map();
-            map.PrintMap();
+            Program[,] programs = new Program[,] { { new Program() }, { new Program() } };
+            foreach (Program program in programs)
+            {
+                program.a = "new string a";
+                program.b += " new";
+            }
+            for (int i = 0; i < programs.GetLength(0); i++)
+            {
+                for (int s = 0; s < programs.GetLength(1); s++)
+                {
+                    Console.WriteLine(programs[i, s].a);
+                }
+            }
+            //Array.ForEach<Program[]>(programs, x =>
+            //{
+            //    Array.ForEach<Program>(x, v => Console.WriteLine($"{v.a}\n{v.b}\n{v.c}"));
+            //});
+
+
+            //Map map = new Map();
+            //map.PrintMap();
         }
     }
 }
