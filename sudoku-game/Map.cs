2ff9688be1b048b4713efd0cbf83e082cd62dedb
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-04-01 14:04:07 -0700

    Commited to code on another computer

--- sudoku-game/Map.cs
+++ sudoku-game/Map.cs
@@ -2,7 +2,8 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using System.Threading.Tasks;
 namespace sudoku_game
 {
     /// <summary>
@@ -30,37 +31,65 @@ namespace sudoku_game
         }
         public void PrintMap()
         {
-            Console.Clear();
-            for (int gridRow = 0; gridRow < Grids.Length; gridRow++)
+            //Console.Clear();
+            //for (int gridRow = 0; gridRow < Grids.GetLength(0); gridRow++)
+            //{
+            //    Console.WriteLine("------------");
+            //    for (int gridCol = 0; gridCol < Grids.GetLength(1); gridCol++)
+            //    {
+            //        Console.Write($"[{gridRow * 3 + gridCol+1}]");
+            //        int rowLength = Grids[gridRow, gridCol].Tiles.GetLength(0);
+            //        int colLength = Grids[gridRow, gridCol].Tiles.GetLength(1);
+
+            //        // printing tiles in the current grid
+            //        for (int row = 0; row < rowLength; row++) // tile row
+            //        {
+            //            for (int col = 0; col < colLength; col++) // tile col
+            //            {
+            //                int? tile = Grids[gridRow, gridCol].Tiles[row, col];
+            //                Console.Write(tile is null ? "-" : tile.ToString());
+            //                var something = tile.ToString();
+            //            }
+            //            Console.Write(row + 1 == rowLength ? "|\n" : "|");
+            //            //Console.WriteLine('|');
+            //        }
+            //    }
+            //}
+
+            //for (int gridRow = 0; gridRow < Grids.GetLength(0); gridRow++)
+            //{
+            //    int counter = 0;
+            //    while (counter++ < Grid._gridsAcross)
+            //    {
+            //        for (int gridCol = 0; gridCol < Grids.GetLength(1); gridCol++)
+            //        {
+            //            Console.Write($"[{gridRow * 3 + gridCol + 1}]");
+            //            for (int row = 0; row < Grid._tilesAcross; row++) // tile row
+            //            {
+            //                for (int col = 0; col < Grid._tilesAcross; col++)
+            //                {
+            //                    int? tile = Grids[gridRow, gridCol].Tiles[row, col];
+            //                    Console.Write(tile is null ? "-" : tile.ToString());
+            //                }
+            //                Console.Write(counter + 1 == Grid._tilesAcross ? "|\n" : "|");
+            //            }
+            //        }
+            //    }
+            //}
+            for (int gridRow = 0; gridRow < Grids.GetLength(0); gridRow++)
             {
-                for (int gridCol = 0; gridCol < Grids[gridCol, gridCol].Tiles.Length; gridCol++)
-                {
-                    for (int row = 0; row < Grids[gridRow, gridCol].Tiles.Length; row++) // tile row
-                    {
-                        for (int col = 0; col < Grids[gridRow, gridCol].Tiles.Length; col++) // tile col
-                        {
-                            int? tile = Grids[gridRow, gridCol].Tiles[row, col];
-                            Console.WriteLine(tile is null ? ' ' : 3);
-                            // jumpnig on the new row
-                            if (col + 1 == Grids[gridRow, gridCol].Tiles.Length)
-                            {
-                                Console.WriteLine('|');
-                            }
-                        }
-                    }
-                }
+
             }
-        }
         public class Grid
         {
             private static Random random = new Random();
 
             private static Grid[,] grids;
-            private static readonly int _gridAmount = 9;
-            private static readonly int _gridsAcross = (int)Math.Sqrt(_gridAmount);
+            internal static readonly int _gridAmount = 9;
+            internal static readonly int _gridsAcross = (int)Math.Sqrt(_gridAmount);
 
-            private static readonly int _tileAmount = 9;
-            private static readonly int _tilesAcross = (int)Math.Sqrt(_tileAmount);
+            internal static readonly int _tileAmount = 9;
+            internal static readonly int _tilesAcross = (int)Math.Sqrt(_tileAmount);
             private int?[,] _tiles;
             public int?[,] Tiles
             {
@@ -69,6 +98,7 @@ namespace sudoku_game
 
             private Grid()
             {
+                Console.WriteLine("Instantiating Grid Class");
                 _tiles = new int?[_tilesAcross,_tilesAcross];
             }
             /// <summary>
@@ -77,6 +107,7 @@ namespace sudoku_game
             /// <returns> An array of grid objects with non-repeating tile numbers: horizontally and vertically (b</returns>
             internal static Grid[,] CreateGrids()
             {
+                Console.WriteLine("Creating Grid...");
                 grids = new Grid[_gridsAcross, _gridsAcross];
                 for (int row = 0; row < _gridsAcross; row++)
                 {
@@ -85,74 +116,68 @@ namespace sudoku_game
                         grids[row, col] = new Grid(); 
                     }
                 }
-                ///<summary>
-                /// add numbers to regex with this method to ignore them
-                /// while assigning numbers to tiles
-                ///</summary>
-                void AddToEndRegex<Nullable>(ref Regex regex, Nullable adding) 
-                {
-                    regex = new Regex(
-                                    regex.ToString().Substring(0, regex.ToString().Length - 1)
-                                    + adding + ']');
-                }
-
+                int count = 0;
+                Console.WriteLine("Start filling the Grids");
                 foreach (Grid grid in grids)
                 {
-                    
-                }
-                ///<summary>
-                ///// In each grid, fills up few random tiles with random numbers
-                ///// </summary>
-                //for (int gridRow = 0; gridRow < _gridsAcross; gridRow++) //loops through grid rows
-                //{
-                //    for (int gridCol = 0; gridCol < _tilesAcross; gridCol++) //loops through grid rows
-                //    {
-                //        Regex numberExceptions = new Regex("[.]");
-                //        bool repeatGrid = false; // if a number can't be in a tile, repeat the loop above
-                //        for (int s = 0; s < random.Next(5); s++) //loops how many numbers to put
-                //        {
-                //            int row = random.Next(1, _tilesAcross);
-                //            int col = random.Next(1, _tilesAcross);
-                //            for (int rowCheck = 0; rowCheck < _tilesAcross; rowCheck++) // checking if row is occupied
-                //            {
-                //                for (int colCheck = 0; colCheck < _tilesAcross; colCheck++) // checking if col is occupied
-                //                {
-                //                    try
-                //                    {
-                //                        int? currentTile = grids[gridRow, gridCol]._tiles[row, col];
-                //                        int? toCheckTile = grids[gridRow, gridCol]._tiles[rowCheck, colCheck];
+                    Console.WriteLine($"\nCreating {++count}th grid----------------");
+                    List<string> tilesToSkip = new List<string>();
+                    List<int?> numbersToSkip = new List<int?>();
 
-                //                        // and tiles shouldn't be equal horizontally and vertically!
-                //                        if (currentTile == toCheckTile) 
-                //                        {
-                //                            gridRow--;
-                //                            repeatGrid = true;
-                //                            break;
-                //                        }
-                //                    }
-                //                    catch (IndexOutOfRangeException ex) // still trying to understand why I have try-catch… 
-                //                    {
-                //                        Console.WriteLine(ex.ToString());
-                //                        continue;
-                //                    }
-                //                }
-                //                if (repeatGrid)
-                //                { break; }
-                //            }
-                //            if (repeatGrid)
-                //            { break; }
-                //            // Assigning a number to a tile
-                //            int? numberToPut;
-                //            do
-                //            {
-                //                numberToPut = random.Next(1, _tileAmount + 1);
-                //            } while (numberExceptions.IsMatch(numberToPut.ToString()));
-                //            AddToEndRegex(ref numberExceptions, numberToPut);
-                //            Console.WriteLine(numberExceptions);
-                //            grids[gridRow, gridCol]._tiles[row, col] = numberToPut;
-                //        }
-                //    }
-                //}
+                    int tilesToFill = random.Next(5);
+                    Console.WriteLine($"Tile Amount: {tilesToFill}");
+                    for (int pos = 0; pos < tilesToFill; pos++) // variable in for loop determines how many positions will be filled
+                    {
+                        Console.WriteLine($"\nFilling {pos + 1}th position");
+                        // checking for uniqueness of position of a tile
+                        int row = -1; 
+                        int col = -1; // is -1 because these had to be assigned to something, I chose -1
+                        string checkUniqueTile = null;
+                        try
+                        {
+                            do
+                            {
+                                row = random.Next(_tilesAcross);
+                                col = random.Next(_tilesAcross);
+                                checkUniqueTile = $"{row},{col}";
+                                Console.WriteLine($"choosing position {checkUniqueTile}");
+                            } while (tilesToSkip.Contains(checkUniqueTile));
+                        }
+                        catch (StackOverflowException ex)
+                        {
+                            Console.WriteLine("Stack overflow when choosing position " + ex);
+                            CreateGrids(); // Restart the method
+                        }
+                        finally
+                        {
+                            Console.WriteLine($"chose position {checkUniqueTile}");
+                            tilesToSkip.Add(checkUniqueTile);
+                        }
+
+                        // giving a unique number to that tile
+                        int? number = null;
+                        try
+                        {
+                            do
+                            {
+                                number = random.Next(1, _tileAmount + 1);
+                                Console.WriteLine($"choosing number {number}");
+                            } while (numbersToSkip.Contains(number));
+                        }
+                        catch (StackOverflowException ex)
+                        {
+                            Console.WriteLine("Stack overflow when choosing number" + ex);
+                            CreateGrids(); // Restart the method
+                        }
+                        finally
+                        {
+                            Console.WriteLine($"chose number {number}");
+                            numbersToSkip.Add(number);
+                            grid._tiles[row, col] = number;
+                        }
+                        Console.WriteLine($"Added {pos + 1}th position; number {number} at [{row},{col}]");
+                    }
+                }
                 return grids;
             }
         }
